6cc0b575d46fdc5dd26c9c353aef6a50a6b98188
douglaslz <44685128+douglaslz@users.noreply.github.com>
2019-08-26 02:13:43 -0400

    Finished httpgets and one post

--- WebApiBooks/Controllers/AuthorController.cs
+++ WebApiBooks/Controllers/AuthorController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiBooks.Contexts;
+using WebApiBooks.Entities;
 
 namespace WebApiBooks.Controllers
 {
@@ -10,6 +12,45 @@ namespace WebApiBooks.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+
+        private readonly ApplicationDBContext context;
+
+        public AuthorController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        //get api authors
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> Get()
+        {
+            return context.Authors.ToList();
+        }
+
+        //get api author 5
+        [HttpGet("{id}")]
+        public ActionResult<Author> Get(int id)
+        {
+            var author = context.Authors.FirstOrDefault(x => x.Id == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+        
+        //post api authors
+        [HttpPost]
+        public ActionResult Post([FromBody] Author author)
+        {
+            context.Add(author);
+            context.SaveChanges();
+            return new CreatedAtRouteResult ("Obtain author", author);
+            
+
+        }
 
     }
 }
