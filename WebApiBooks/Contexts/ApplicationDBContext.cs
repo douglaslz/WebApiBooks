2e5114e25dac1faceb2660dc3234a4b7639793dc
douglaslz <44685128+douglaslz@users.noreply.github.com>
2019-08-25 23:39:46 -0400

    First Part

---
+++ WebApiBooks/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiBooks.Entities;
+
+namespace WebApiBooks.Contexts
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
